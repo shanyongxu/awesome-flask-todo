377f8f451df99a1a2bd9d104535312cae7102446
syx <783284481@qq.com>
2017-07-11 10:03:09 +0800

    v0.3 database

--- manager.py
+++ manager.py
@@ -1,10 +1,16 @@
 #-*- conding:utf-8
 
-from flask.ext.script import Manager, Server
+from flask_script import Manager, Server
 from app import app 
+from app.models import *
 manager = Manager(app)
 
 manager.add_command('runserver',Server(host='syx666.com',port=8090,use_debugger=True))
+
+@manager.command
+def save_todo():
+    todo = Todo(content='my first todo')
+    todo.save()
 
 if __name__ == '__main__':
     manager.run()
